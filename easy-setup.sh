f0ee80583df38a6c9a667135441267b65503b474
Shaun The Salvo <110704527+ShaunTheSalvo@users.noreply.github.com>
2025-02-21 14:55:43 +1000

    Updated version of script
    Added andontie-aur repo to standard installation, to provide access to more precompiled binaries - eg snapd

--- easy-setup.sh
+++ easy-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-echo -e "$(tput bold)Arch Easy Setup Script: V1.3 - 20231223$(tput sgr0)" ; sleep 2
+SCRIPTVER="Arch Easy Setup Script: V1.7 - 20250213"
+echo -e "$(tput bold)$SCRIPTVER$(tput sgr0)" ; sleep 2
 
-whiptail --title "Arch Easy Setup Script: V1.3 20231223" --msgbox "Hello, and thank you for using my Arch Linux Easy Setup Script. This script allows installing from a choice of minimal desktops, as well as some useful tools, apps and utilities. In the next few screens, you can select what you'd like to install. After that, the rest of the script is fully automatic, so just sit back and relax while I take care of the magic. :)" 16 50
+whiptail --title "$SCRIPTVER" --msgbox "Hello, and thank you for using my Arch Linux Easy Setup Script. This script allows installing from a choice of minimal desktops, as well as some useful tools, apps and utilities. In the next few screens, you can select what you'd like to install. After that, the rest of the script is fully automatic, so just sit back and relax while I take care of the magic. :)" 16 50
 
 ############################
 ### Set user preferences ###
@@ -15,8 +16,8 @@ if whiptail --title "Link /home/$USER dirs in /home/common" --yesno "Would you l
 	COMMON=1
 fi
 
-if [[ ! -f /usr/bin/yay ]]; then
-	REPOS=1
+if whiptail --title "Add custom repos" --yesno "Would you like to add custom repos (Chaotic-AUR, Andontie-AUR and Systemback)?" 8 50 ; then
+REPOS=1
 fi
 
 if [[ $REPOS == 1 ]]; then
@@ -30,6 +31,7 @@ DESKTOP=$(whiptail --title "Desktop Installation Selection" --menu "Which deskto
 "Gnome" "" \
 "Cinnamon" "" \
 "Budgie" "" \
+"Cosmic (EXPERIMENTAL!)" "" \
 "None at this time" "" 3>&1 1>&2 2>&3)
 
 BROWSER=$(whiptail --title "Web Browser Selection" --menu "Which web browser would you like to install?" 16 30 6 \
@@ -45,14 +47,14 @@ if [[ ! $BROWSER == N* ]]; then
 	fi
 fi
 
-GENERAL1=$(whiptail --title "General Applications & Libraries" --checklist "Which general apps and libraries would you like to install? Note: if you change options such as mesa and vulkan, some games and programs may not run if they are not installed! Select CANCEL if you don't want to install anything here. Note: basic packages such as CUPS, and support for NTFS and FAT filesystems are installed by default" 34 40 20 \
+GENERAL1=$(whiptail --title "General Applications & Libraries" --checklist "Which general apps and libraries would you like to install? Note: if you change options such as mesa and vulkan, some games and programs may not run if they are not installed! Select CANCEL if you don't want to install anything here. Note: basic packages such as CUPS, and support for NTFS and FAT filesystems are installed by default" 34 41 20 \
 libreoffice-fresh "" ON \
 wps-office "" OFF \
 ttf-ms-fonts "" OFF \
 hplip "" OFF \
 vlc "" ON \
 lutris "" OFF \
-wine "" ON \
+wine-stable "" ON \
 winetricks "" ON \
 vulkan-icd-loader "" ON \
 lib32-vulkan-icd-loader "" ON \
@@ -66,6 +68,7 @@ lib32-openal "" ON \
 mesa "" ON \
 lib32-mesa "" ON \
 downgrade "" ON \
+octopi "" ON \
 3>&1 1>&2 2>&3)
 GENERAL=$(echo $GENERAL1 | tr -d "\"")
 
@@ -73,15 +76,22 @@ GENERAL=$(echo $GENERAL1 | tr -d "\"")
 ### Confirm all okay before proceeding ###
 ##########################################
 
-if whiptail --title "Arch Easy Setup Script" --yesno "Would you like the script to run entirely automatically, or confirm each installation step?" 8 55 ; then
-	$CONFIRM="--noconfirm"
+if whiptail --title "$SCRIPTVER" --yesno "Would you like the script to run entirely automatically (YES), or confirm each installation step (NO)?" 9 55 ; then
+	CONFIRM=--noconfirm
 	else
-	$CONFIRM="--canfirm"
+	CONFIRM=--confirm
 fi
-if ! whiptail --title "Arch Easy Setup Script" --yesno "Okay, that's all the information I need. Would you like to continue?" 8 50 ; then
-	whiptail --title "Arch Easy Setup Script" --msgbox "That's not a problem. You can run the script again if you wish. Have a nice day!" 8 50;  exit 0
+
+if ! whiptail --title "$SCRIPTVER" --yesno "Okay, that's all the information I need. Would you like to continue?" 8 50 ; then
+	whiptail --title "$SCRIPTVER" --msgbox "That's not a problem. You can run the script again if you wish. Have a nice day!" 8 50;  exit 0
 fi
 clear
+
+###################################################
+### Make sure pacman up to date before starting ###
+###################################################
+
+sudo pacman -Syyu
 
 ##########################
 ### Add bash shortcuts ###
@@ -89,7 +99,7 @@ clear
 
 if [[ $SHORT == 1 ]]; then
 	echo -e "\n\n$(tput bold)Adding bash shortcuts$(tput sgr0)"
-	echo -e "alias ls='ls --color=auto -alh'\nalias install='yay -S'\nalias remove='yay -Rcns'\nalias autoremove='yay -Rcns $(yay -Qdttq)'\nalias update='yay -Syu'\nalias search='yay -Ss'\nalias showinst='yay -Qs'\nalias edit='kate'\nalias wipe='yes|yay -Scc >/dev/null ; rm -rf ~/.cache/yay ; rm /home/$USER/.bash_history ; clear ; history -c'\nalias updategrub='sudo grub-mkconfig -o /boot/grub/grub.cfg'\nalias reflector='sudo reflector -c AU --save /etc/pacman.d/mirrorlist'" >> /home/$USER/.bashrc
+	echo -e "alias ls='ls --color=auto -alh'\nalias install='yay -S'\nalias remove='yay -Rcns'\nalias autoremove='yay -Rcns $(yay -Qdttq)'\nalias update='yay -Syu'\nalias search='yay -Ss'\nalias showinst='yay -Qs'\nalias edit='xed'\nalias wipe='yes|yay -Scc >/dev/null ; rm -rf ~/.cache/yay ; rm /home/$USER/.bash_history ; clear ; history -c'\nalias updategrub='sudo grub-mkconfig -o /boot/grub/grub.cfg'\nalias reflector='sudo reflector -c AU --save /etc/pacman.d/mirrorlist'" >> /home/$USER/.bashrc
 fi
 
 ################################
@@ -194,29 +204,25 @@ if [[ $REPOS == 1 ]]; then
 	fi
 	
 	# Add Chaotic-AUR entries to pacman.conf
-	echo -e "\n\n$(tput bold)Adding chaotic-AUR to pacman.conf$(tput sgr0)"
+	echo -e "\n\n$(tput bold)Adding chaotic-AUR and Andontie-AUR to pacman.conf$(tput sgr0)"
 	
+	# Chaotic-AUR
 	sudo pacman-key --recv-key 3056513887B78AEB
 	sudo pacman-key --lsign-key 3056513887B78AEB
 	sudo pacman -U $CONFIRM 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst'
 	sudo pacman -U $CONFIRM 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
 	echo -e '\n[chaotic-aur]\nInclude = /etc/pacman.d/chaotic-mirrorlist' >> ./pacman.conf
 	
+	# Andontie-AUR
+	sudo pacman-key --recv-key 72BF227DD76AE5BF
+	sudo pacman-key --lsign-key 72BF227DD76AE5BF
+	echo -e '\n[andontie-aur]\nServer = https://aur.andontie.net/$arch' >> ./pacman.conf
+	
 	sudo cp -r ./pacman.conf /etc; rm ./pacman.conf
 	
 	# Install yay
 	sudo pacman -Sy $CONFIRM yay
 fi
-
-##########################
-### Install Systemback ###
-##########################
-
-if [[ $SYSBACK == 1 ]]; then
-	echo -e "\n\n$(tput bold)Installing Systemback$(tput sgr0)"
-	yay -S $CONFIRM systemback
-fi
-
 
 ############################
 ### Desktop Installation ### 
@@ -226,7 +232,7 @@ fi
 ### Install minimal KDE ###
 if [[ $DESKTOP == K* ]]; then
 	echo -e "\n\n$(tput bold)Installing minimal KDE Desktop$(tput sgr0)"
-	yay -S $CONFIRM sddm sddm-kcm breeze-gtk networkmanager nm-connection-editor plasma-nano plasma-wayland-session plasma-nm plasma-pa kdeplasma-addons discover packagekit-qt5 kde-gtk-config systemsettings powerdevil bluez bluedevil konsole dolphin yakuake kscreen kmix kate mc kio-gdrive plasma5-applets-kde-arch-update-notifier wget
+	yay -S $CONFIRM sddm sddm-kcm breeze-gtk kde-gtk-config networkmanager nm-connection-editor plasma-desktop plasma-wayland-session plasma-nm plasma-pa kdeplasma-addons systemsettings powerdevil bluez bluedevil konsole dolphin yakuake kscreen kmix packagekit featherpad mc rclone wget
 	sudo systemctl enable sddm
 fi
 
@@ -234,48 +240,54 @@ fi
 if [[ $DESKTOP == G* ]]; then
 	echo -e "\n\n$(tput bold)Installing minimal Gnome Desktop and extensions$(tput sgr0)"
 	yay -S $CONFIRM gdm wget
-	yay -S $CONFIRM gnome-shell gnome-terminal gnome-tweak-tool gnome-control-center xdg-user-dirs xdg-desktop-portal-gnome eog gnome-menus gnome-browser-connector gnome-screenshot gnome-shell-extensions gnome-tweaks nemo nemo-terminal gnome-themes-extra extension-manager gnome-shell-extension-arc-menu gnome-shell-extension-gtk4-desktop-icons-ng gnome-shell-extension-arch-update gnome-shell-extension-bing-wallpaper gnome-shell-extension-dash-to-panel gnome-shell-extension-weather-oclock-git guake alacarte mc kate evince seahorse qgnomeplatform-qt5 qgnomeplatform-qt6 qt5-wayland qt6-wayland gnome-keyring gvfs-google seahorse # gdm gnome-shell-extension-smart-auto-move-git ## disabled as extension not compatible with Gnome 45
+	yay -S $CONFIRM gnome-shell gnome-terminal gnome-tweak-tool gnome-control-center xdg-user-dirs xdg-desktop-portal-gnome eog gnome-menus gnome-browser-connector gnome-screenshot gnome-shell-extensions gnome-tweaks nemo nemo-terminal gnome-themes-extra extension-manager guake alacarte mc xed evince seahorse gnome-keyring rclone seahorse qt5-wayland-decorations qt6-wayland-decorations gnome-shell-extension-arc-menu gnome-shell-extension-gtk4-desktop-icons-ng gnome-shell-extension-arch-update gnome-shell-extension-bing-wallpaper gnome-shell-extension-dash-to-panel
 
-	# Install beta/test version of Smart Auto Move - props to ionutbortis, nice work mate! :)
-	wget https://github.com/khimaros/smart-auto-move/files/13418637/smart-auto-move%40khimaros.com.shell-extension.zip
-	gnome-extensions install --force smart-auto-move@khimaros.com.shell-extension.zip
 
 	sudo systemctl enable gdm
 
-	# Disable power-off and logout confirmation prompts (60 second timer)
+	# Install extensions
+#	echo -e "\n\n$(tput bold)Installing and activating Gnome extensions$(tput sgr0)"
+#    yes|gext install arch-update@RaphaelRochet arcmenu@arcmenu.com dash-to-panel@jderose9.github.com  smart-auto-move@khimaros.com gtk4-ding@smedius.gitlab.com weatheroclock@CleoMenezesJr.github.io
+
+    gnome-extensions enable arch-update@RaphaelRochet
+    gnome-extensions enable arcmenu@arcmenu.com
+    gnome-extensions enable dash-to-panel@jderose9.github.com
+    gnome-extensions enable ding@rastersoft.com
+    gnome-extensions enable smart-auto-move@khimaros.com
+    gnome-extensions enable weatheroclock@CleoMenezesJr.github.io
+
+	# Disable timeout,power-off and logout confirmation prompts
 	gsettings set org.gnome.SessionManager logout-prompt false
-	
-	# Enable extensions
-	echo -e "\n\n$(tput bold)Activating installed Gnome extensions$(tput sgr0)"
-	gnome-extensions enable arch-update@RaphaelRochet
-	gnome-extensions enable arcmenu@arcmenu.com
-	gnome-extensions enable dash-to-panel@jderose9.github.com
-	gnome-extensions enable ding@rastersoft.com
-	gnome-extensions enable smart-auto-move@khimaros.com
-	gnome-extensions enable weatheroclock@CleoMenezesJr.github.io
+	gsettings set org.gnome.mutter check-alive-timeout 0
 fi
 
 ### Install minimal Cinnamon ###
-if [[ $DESKTOP == C* ]]; then
+if [[ $DESKTOP == Ci* ]]; then
 	echo -e "\n\n$(tput bold)Installing Cinnamon Desktop$(tput sgr0)"
-	yay -S $CONFIRM cinnamon lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings featherpad nemo-terminal gnome-terminal wget
+	yay -S $CONFIRM cinnamon lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings xed nemo-terminal gnome-terminal wget
 	sudo systemctl enable lightdm
 fi
 
 ### Install minimal Budgie ###
 if [[ $DESKTOP == B* ]]; then
 	echo -e "\n\n$(tput bold)Installing Budgie Desktop$(tput sgr0)"
-	yay -S $CONFIRM budgie-desktop lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit wget
+	yay -S $CONFIRM budgie-desktop lightdm lightdm-gtk-greeter lightdm-gtk-greeter-settings gedit wget xed
 	sudo systemctl enable lightdm
 fi
 
+### Install minimal Cosmic ###
+if [[ $DESKTOP == Co* ]]; then
+	echo -e "\n\n$(tput bold)Installing Cosmic Desktop$(tput sgr0)"
+	yay -S $CONFIRM gdm cosmic-session
+	sudo systemctl enable gdm
+fi
 ################################
 ### Install general software ###
 ################################
 
 	echo -e "\n\n$(tput bold)Installing general software and libraries$(tput sgr0)"
 	echo $GENERAL
-	yay -S $CONFIRM system-config-printer cups libdvdread libdvdcss libcdio exfat-utils ntfs-3g sof-firmare $GENERAL
+	yay -S $CONFIRM nano system-config-printer cups libdvdread libdvdcss libcdio exfat-utils ntfs-3g sof-firmare $GENERAL
 	sudo systemctl enable cups.service; sudo systemctl start cups.service
 
 
@@ -304,6 +316,15 @@ if [[ $BROWSERINST == 1 ]]; then
 		echo -e "\n\n$(tput bold)Installing Vivaldi as default web browser$(tput sgr0)"
 		yay -S $CONFIRM vivaldi
 	fi
+fi
+
+##########################
+### Install Systemback ###
+##########################
+
+if [[ $SYSBACK == 1 ]]; then
+	echo -e "\n\n$(tput bold)Installing Systemback$(tput sgr0)"
+	yay -S $CONFIRM systemback
 fi
 
 
